68f6b09cb0b6c627e7b2e8e23712792b11a85a00
power81 <134614142+power81@users.noreply.github.com>
2023-05-25 18:06:08 +0530

    web service

---
+++ p7 gh.py
@@ -0,0 +1,46 @@
+'''7. Create a simple web service and write any distributed application to consume the 
+web service.'''
+
+#web_service2.py
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify(message='Hello, World!')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+#distributed_application2.py
+import requests
+import multiprocessing
+import os
+
+def consume_web_service(app_id):
+    url = f'http://127.0.0.1:5000/api/hello?app_id={app_id}'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        message = data['message']
+        print(f'Response from App ID {app_id}:', message)
+    else:
+        print(f'Failed to fetch data from the web service for App ID {app_id}')
+
+if __name__ == '__main__':
+    num_instances = 5  # Number of distributed application instances to run
+
+    processes = []
+    for i in range(num_instances):
+        process = multiprocessing.Process(target=consume_web_service, args=(i,))
+        process.start()
+        processes.append(process)
+
+    for process in processes:
+        process.join()
